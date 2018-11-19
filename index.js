45fe12d04f175859f1037bf9730ee9d407fae704
elisabetros <32452401+elisabetros@users.noreply.github.com>
2018-11-19 12:05:44 +0100

    service worker

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+"user strict";
+window.onload= function() => {
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker
+      .register("./service-worker.js", { scope: "./about" })
+      .then(function(registration) {
+        console.log(registration);
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+  } else {
+    console.log("Service Worker is not supported in this browser.");
+  }}
+
 let endpoint =
   "http://ekaterinagp.dk/kea/wordpress/wp-json/wp/v2/opskrifter?_embed&fbclid=IwAR2yUdAUNX80ukrXBprITALfBQdoLnWY6y_1TTuLvE54pg7Y-Qe4fEA_4w4&per_page=100";
 let section = document.querySelector("section");
