a494c30e37b01f2c21ce4cf59e88c079e320e304
elisabetros <32452401+elisabetros@users.noreply.github.com>
2018-11-19 11:48:37 +0100

    fetching titles

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-let endpoint = "http://www.recipepuppy.com/api/?&q=cake&p=2";
-let body = document.querySelector("body");
+let endpoint =
+  "http://ekaterinagp.dk/kea/wordpress/wp-json/wp/v2/opskrifter?_embed&fbclid=IwAR2yUdAUNX80ukrXBprITALfBQdoLnWY6y_1TTuLvE54pg7Y-Qe4fEA_4w4&per_page=100";
+let section = document.querySelector("section");
+let template = document.querySelector("template").content;
 
 fetch(endpoint)
   .then(res => res.json())
   .then(data => {
-    console.log(data);
-    displayCat(data);
+    displayRecipes(data);
   });
 
-function displayCat(link) {
-  let img = document.createElement("img");
-  img.setAttribute("src", link.file);
-  body.appendChild(img);
+function displayRecipes(recipes) {
+  //   console.log(recipes);
+  recipes.forEach(recipe => {
+    const clone = template.cloneNode(true);
+    clone.querySelector("h1").textContent = recipe.title.rendered;
+    clone.querySelector("img");
+    section.appendChild(clone);
+  });
 }
